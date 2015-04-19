387034d10b43bfc06f3b98ffbd3485dd155ff20f
Avi Aialon <aviaialon@gmail.com>
2015-04-19 09:22:41 -0400

    Cleanup of unused files

--- mvc/partial/menu/category-selector.php
+++ mvc/partial/menu/category-selector.php
@@ -19,40 +19,18 @@
                     <a href="#">(<?php echo($Application->translate('View All', 'Tout Voire', 'View All')); ?>)</a>
                   </div>
                   <?php if (empty($categoryData['children']) === false) { ?>
-                  <?php /*?><ul>
-                  	<?php $cnt 	 = 0; ?>
-                  	<?php $clpId = time(); ?>
-                    <?php foreach ($categoryData['children'] as $childCatId => $childCatData) { ?>
-                    	<?php if ($cnt === 4) { ?>
-                        	<li class="collapse" id="<?php echo ($clpId); ?>"><ol>
-						<?php } ?>
-                    	<li><a href="#"><?php echo ucwords($Application->translate($childCatData['name_en'], $childCatData['name_fr'], $childCatData['name_ch'])); ?></a>
-                        	<span><?php echo $childCatData['catCount']; ?></span></li>
-                        <?php $cnt++; ?>
-                    <?php } ?>
-                    <?php if ($cnt >= 4) { ?>
-                    	</ol>
-                      </li>
-                      
-                        <a class="cs-link-more collapsed"
-                        	data-toggle="collapse" href="#<?php echo $clpId; ?>" data-target="#<?php echo $clpId; ?>" 
-                            	aria-expanded="false" aria-controls="<?php echo $clpId; ?>">
-                            	<i class="icon-plus8"></i>more categories</a>
-					<?php } ?>
-                    
-                  </ul><?php */?>
                   <ul>
                   	<?php $cnt 	 = 0; ?>
                   	<?php $clpId = mt_rand(); ?>
                     <?php foreach ($categoryData['children'] as $childCatId => $childCatData) { ?>
-                    	<?php if ($cnt === 4 && count($categoryData['children']) > 5) { ?>
+                    	<?php if ($cnt === 4 && count($categoryData['children']) >= 5) { ?>
                         	<section class="collapse" id="<?php echo $clpId; ?>">
 						<?php } ?>
                     	<li><a href="#"><?php echo ucwords($Application->translate($childCatData['name_en'], $childCatData['name_fr'], $childCatData['name_ch'])); ?></a>
                         	<span><?php echo $childCatData['catCount']; ?></span></li>
                         <?php $cnt++; ?>
                     <?php } ?>
-                    <?php if (count($categoryData['children']) > 5) { ?>
+                    <?php if (count($categoryData['children']) >= 5) { ?>
                     	</section>
                       
                         <a class="cs-link-more" href="#<?php echo $clpId; ?>" data-target="#<?php echo $clpId; ?>" 
@@ -71,22 +49,3 @@
       </div>
     </div>
 </div>
-
-<?php /*
-<li class="col-md-3">
-                  <div class="cat-inner"> <img src="http://directory.chimpgroup.com/wp-content/uploads/Pets1.png" alt="" /> <a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;submit=">Pets</a> <a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;submit=">(View All)</a> </div>
-                  <ul>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=birds&amp;submit=">Birds</a><span>2</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=cats&amp;submit=">Cats</a><span>1</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=dogs&amp;submit=">Dogs</a><span>2</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=equipment&amp;submit=">Equipment</a><span>2</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=exotics&amp;submit=">Exotics</a><span>2</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=fish&amp;submit=">Fish</a><span>1</span></li>
-                    <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=missing-found&amp;submit=">Missing &amp; Found</a><span>2</span></li>
-                    <li class="collapse" id="1568">
-                      <ol>
-                        <li><a href="http://directory.chimpgroup.com/listing-plain-heading/?&amp;filter=all&amp;type=pets&amp;directory_categories=pets-for-sale&amp;submit=">Pets for Sale</a><span>1</span></li>
-                      </ol>
-                    </li>
-                  </ul>
-                  <a class="cs-link-more cs-link-more-1568 collapsed" onclick="cs_slide_toogle(this.getAttribute('class'),1568)" data-toggle="collapse" href="#1568" aria-expanded="false" aria-controls="1568"> <i class="icon-plus8"></i>more categories</a></li>*/ ?>
